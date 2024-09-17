c95e99194b4f2c9f680e7e60864f99f06c74a6e8
nohamostafa123 <nohamoustafa2003@gmail.com>
2024-09-17 18:51:29 +0300

    changed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rjshkhr/cleanfolio'
+      href='https://github.com/nohamostafa123/myy-portfolio'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+      Created By Noha Mostafa
     </a>
   </footer>
 )
